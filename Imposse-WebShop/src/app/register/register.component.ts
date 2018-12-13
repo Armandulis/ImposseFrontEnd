46054497b786d9e2d4ce5de7a1649823446cf5ed
Ingvar Friðbjörnsson <ingvarfr@gmail.com>
2018-12-13 17:36:01 +0100

    debugger fix

--- Imposse-WebShop/src/app/register/register.component.ts
+++ Imposse-WebShop/src/app/register/register.component.ts
@@ -29,7 +29,6 @@ export class RegisterComponent implements OnInit {
   ngOnInit() {
     this.userService.getUsernames().subscribe( data => {
       this.usernames = data;
-      debugger;
     });
   }
 
